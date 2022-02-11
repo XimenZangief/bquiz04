065d6d8adec62789bf730994a3d0ff891bb5ed87
XimenZangief <s9671073@gmail.com>
2022-02-11 11:17:51 +0800

    增加跑馬燈 增加題目要求的圖片 建置news內容

---
+++ front/news.php
@@ -0,0 +1,16 @@
+<h1 class="ct">最新消息</h1>
+
+
+<!-- emmet打法  -->
+<!-- table.all>tr.tt.ct+tr.pp*2  -->
+<table class="all">
+    <tr class="tt ct">
+        <td>標題</td>
+    </tr>
+    <tr class="pp">
+        <td>情人節特惠活動</td>
+    </tr>
+    <tr class="pp">
+        <td>年終特賣會開跑了</td>
+    </tr>
+</table>
