71ed144e8192c75faa0381faaff8062b2b510398
XimenZangief <s9671073@gmail.com>
2022-02-15 11:37:54 +0800

    優化-兩個自訂script做合併成newType(type)，使用switch判別載入的值 將api內的get_big和get_mid合併成save_type.php

---
+++ api/save_type.php
@@ -0,0 +1,6 @@
+<?php
+include_once "../base.php";
+// 回傳受影響的資料筆數
+// echo $Type->save($_POST);
+$Type->save($_POST);
+?>
--- back/th.php
+++ back/th.php
@@ -2,13 +2,13 @@
 <div>
     新增大分類
     <input type="text" name="big" id="big">
-    <button onclick="newBig()">新增</button>
+    <button onclick="newType('big')">新增</button>
 </div>
 <div>
     新增中分類
     <select name="parent" id="parent"></select>
     <input type="text" name="mid" id="mid">
-    <button onclick="newMid()">新增</button>
+    <button onclick="newType('mid')">新增</button>
 </div>
 
 <!-- 分類區 -->
@@ -44,19 +44,38 @@
 <script>
     $("#parent").load("api/get_big.php");
 
-    function newBig(){
-        let big=$("#big").val();
-        $.post("api/new_big.php",{name:big},(res)=>{
+    function newType(type){
+        let name, parent;
+        switch(type){
+            case "big":
+                name=$("#big").val();
+                parent=0;
+                break;
+            case "mid":
+                name=$("#mid").val();
+                parent=$("#parent").val();
+                break;
+        }
+        $.post("api/save_type.php",{name,parent},(res)=>{
+            // 沒有回傳值所以log一片空白
             // console.log(res);
             location.reload();
         })
     }
-    function newMid(){
-        let parent=$("#parent").val();
-        let mid=$("#mid").val();
-        $.post("api/new_mid.php",{name:mid,parent},(res)=>{
-            // console.log(res);
-            location.reload();
-        })
-    }
+
+    // function newBig(){
+    //     let big=$("#big").val();
+    //     $.post("api/new_big.php",{name:big,parent:0},(res)=>{
+    //         // console.log(res);
+    //         location.reload();
+    //     })
+    // }
+    // function newMid(){
+    //     let parent=$("#parent").val();
+    //     let mid=$("#mid").val();
+    //     $.post("api/new_mid.php",{name:mid,parent},(res)=>{
+    //         // console.log(res);
+    //         location.reload();
+    //     })
+    // }
 </script>
