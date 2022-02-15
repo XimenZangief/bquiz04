056325ee77fe13cc223d69434e3ad29bb479da9c
XimenZangief <s9671073@gmail.com>
2022-02-15 15:46:36 +0800

    優化-將get_big併入get_type並修改相關路徑

--- api/get_big.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "../base.php";
-
-// 找到所有parent=0的大分類，並給予name排序(預設ASC升序)
-$bigs=$Type->all(['parent'=>0],"ORDER BY name");
-foreach ($bigs as $key => $value) {
-    echo "<option value='{$value['id']}'>{$value['name']}</option>";
-}
-?>
